b4edfe92b9e0244ad6b0d088a5c13d25a36fbba0
m_igashi <@M_Igashi>
2025-04-07 23:57:50 +0200

    renameprocess

--- src/components/ui/buttonx.tsx
+++ src/components/ui/buttonx.tsx
@@ -2,6 +2,7 @@
 import * as React from "react";
 import { cva, type VariantProps } from "class-variance-authority";
 
+
 const buttonVariants = cva(
   "inline-flex items-center justify-center rounded-xl text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none px-4 py-2",
   {
